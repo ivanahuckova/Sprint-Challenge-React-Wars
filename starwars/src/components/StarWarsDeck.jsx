6295af9d84440433a85ff022ff10f6f039b23c0b
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-15 09:57:51 +0100

    style: Fix card styling

--- starwars/src/components/StarWarsDeck.jsx
+++ starwars/src/components/StarWarsDeck.jsx
@@ -14,6 +14,9 @@ export default function StarWarsDeck({ char }) {
 			<div>
 				eye color: <span>{char.eye_color}</span>
 			</div>
+			<div className="image-container">
+				<img src="https://cdn.vox-cdn.com/thumbor/UHTSj3_-peudjTCtAEEfH9Rm8E4=/0x0:1536x864/1200x800/filters:focal(646x310:890x554)/cdn.vox-cdn.com/uploads/chorus_image/image/58953911/star_wars_logo_new_tall.0.jpg" />
+			</div>
 		</div>
 	);
 }
