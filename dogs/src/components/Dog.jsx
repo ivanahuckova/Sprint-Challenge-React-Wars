aa9fb6649cfe0b72ff20f8454526abbb1eac04d9
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-15 11:53:48 +0100

    style: Style dog cards info

--- dogs/src/components/Dog.jsx
+++ dogs/src/components/Dog.jsx
@@ -5,6 +5,9 @@ export default function Dog({ dog }) {
 	return (
 		<div className="dog-container">
 			<img src={dog} />
+
+			<div className="dogo-intro">This doggo gets</div>
+			<div className="dogo-rating">{Math.floor(Math.random() * 10) + 11}/10 points!</div>
 		</div>
 	);
 }
